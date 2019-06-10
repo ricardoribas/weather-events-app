8d95a4c3f8008ef9657e788ca8dfcbab0cf90354
Ricardo Ribas <ricardo.ribas.1991@gmail.com>
2019-06-10 21:06:06 +0200

    Beautify list of events

--- client/src/components/events/Events.jsx
+++ client/src/components/events/Events.jsx
@@ -1,57 +1,57 @@
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import {
-  Table
+	Card,
+	Row,
+	Col
 } from 'react-bootstrap';
 import React from 'react';
 import { Link } from "react-router-dom";
 
+import './Events.scss';
 import { createEvent } from './../../redux/actions/events';
 
 const mapStateToProps = state => ({ events: state.events });
 const mapDispatchToProps = dispatch => ({
-  createEvent: params => dispatch(createEvent(params)),
+	createEvent: params => dispatch(createEvent(params)),
 });
 
 function getEvents(events = []) {
-  return events
-    .map((e, index) => (
-      <tr key={index.toString()}>
-        <td></td>
-        <td>{e.title}</td>
-        <td>{e.location}</td>
-        <td>{e.date}</td>
-        <td><Link to={{ pathname: '/events/detail', event: e }}>View</Link></td>
-      </tr>
-    ));
+	return events
+		.map((e, index) => (
+			<Card key={index.toString()} className="Events__Card">
+				<Card.Body>
+					<Card.Title>{e.title}</Card.Title>
+					<Card.Subtitle className="mb-2 text-muted">{e.date}</Card.Subtitle>
+					<Card.Text>
+						@{e.location}
+					</Card.Text>
+					<Link to={{ pathname: '/events/detail', event: e }}>Edit event</Link>
+				</Card.Body>
+			</Card>
+		));
 }
 
 class Events extends React.Component {
-  render() {
-    return (
-      <Table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Title</th>
-            <th>Date</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {getEvents(this.props.events)}
-        </tbody>
-      </Table>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Row>
+					<Col><h1>Ogun Events</h1></Col>
+				</Row>
+				<Row>
+					{getEvents(this.props.events)}
+				</Row>
+			</div>
+		);
+	}
 }
 
 Events.propTypes = {
-  events: PropTypes.arrayOf(Object).isRequired
+	events: PropTypes.arrayOf(Object).isRequired
 };
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps
 )(Events);
