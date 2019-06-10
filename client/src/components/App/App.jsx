c87569919b401e37a52e3463167c240aed9f92e8
Ricardo Ribas <ricardo.ribas.1991@gmail.com>
2019-06-10 20:23:30 +0200

    added navbar

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,15 +1,17 @@
 import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import {
-  Container
+  Container,
+  Navbar,
+  Nav
 } from 'react-bootstrap';
+import './App.scss';
 
 import React, { Component } from 'react';
 
 import Events from '../events';
 import Event from '../event';
 import { getEvents } from '../../redux/actions/events';
-
 
 const mapDispatchToProps = dispatch => ({
   getEvents: params => dispatch(getEvents(params))
@@ -24,17 +26,19 @@ export class App extends Component {
     return (
       <Container className="App">
         <Router>
-          <div>
-            <ul>
-              <li>
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">Ogun recruitment test</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
                 <Link to="/events">Events</Link>
-              </li>
-            </ul>
-            <Route exact path="/events" component={Events} />
-            <Route path="/events/detail" render={(props)=>{
-              return (<Event event={props.location.event} />)
-            } }/>
-          </div>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Route exact path="/events" component={Events} />
+          <Route path="/events/detail" render={(props)=>{
+            return (<Event event={props.location.event} />)
+          } }/>
         </Router>
       </Container>
     );
